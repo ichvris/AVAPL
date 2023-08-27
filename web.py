43f9ebdf41614afe3b67575cfef237edbd06439a
ichvris <133998972+ichvris@users.noreply.github.com>
2023-08-27 13:30:39 +0200

    Update web.py

--- web.py
+++ web.py
@@ -133,7 +133,7 @@ async def main():
     aiohttp_jinja2.setup(app, loader=jinja2.FileSystemLoader("templates"))
     runner = web.AppRunner(app)
     await runner.setup()
-    site = web.TCPSite(runner, "0.0.0.0", int(config["webserver"]["web_port"]))
+    site = web.TCPSite(runner, "185.229.227.61", int(config["webserver"]["web_port"]))
     await site.start()
 
 
